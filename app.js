7e5e16381a1213d61a80415a5ca6fa3ee81e5755
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-05-05 13:31:07 -0500

    Changing the development port, setting the assets dir, views engine and the home page.

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ var app = express();
 
 // Set the port for our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// Set the home page route
+app.get('/', function(req, res) {
+	// Ejs render automatically looks in the views folder
+	res.render('index');
+});
 
 // GET method route
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
 	res.send('GET request to the homepage');
 });
 
