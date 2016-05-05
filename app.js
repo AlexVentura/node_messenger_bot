93ac6fc3bde0a193638a2fe35b193d2ff774e083
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-05-05 13:12:04 -0500

    Using a variable for the PORT, so letting Heroku tu set it too.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// Set the port for our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 // GET method route
 app.get('/', function (req, res) {
 	res.send('GET request to the homepage');
@@ -11,6 +15,6 @@ app.post('/', function (req, res) {
 	res.send('POST request to the homepage');
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+	console.log('Node-Messenger-Bot app is running on http://localhost:' + port);
 });
