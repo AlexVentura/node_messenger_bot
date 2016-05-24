7e66949ad43df11d788e3b9782d93686a45c0d5d
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-05-24 01:00:14 -0500

    Adding the new method to interact with Wiki and the User.

--- services/answers-service.js
+++ services/answers-service.js
@@ -79,3 +79,52 @@ module.exports.sendGenericMessage = (sender) => {
 	});
 
 };
+
+module.exports.sendWikiResultMessage = (sender, criteria) => {
+
+	var messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": []
+			}
+		}
+	};
+
+	var card = {
+		"title": "First card",
+		"subtitle": "Element #1 of an hscroll",
+		"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+		"buttons": [{
+			"type": "web_url",
+			"url": "https://www.messenger.com/",
+			"title": "Web url"
+		}, {
+			"type": "postback",
+			"title": "Postback",
+			"payload": "Payload for first element in a generic bubble",
+		}],
+	};
+
+	wiki.executeSearch(req.query.criteria, (response) => {
+		console.log('▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸', response);
+	});
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: token },
+		method: 'POST',
+		json: {
+			recipient: { id: sender },
+			message: {messageData},
+		}
+	}, (error, response, body) => {
+		if (error) {
+			console.log('▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸ Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸▸ Error: ', response.body.error);
+		}
+	});
+
+};
